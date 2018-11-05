f9ff5aff1630403b712579f74c47d93ab202250e
William Sena Silva <williamsenasilva@yahoo.com>
2018-11-05 01:32:10 -0200

    adicionado suporte a ícones AwesomeIcons

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text, Button, ActivityIndicator, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, ActivityIndicator, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 class Home extends React.Component {
   constructor(props) {
@@ -13,21 +14,25 @@ class Home extends React.Component {
 
   mountScreen() {
     return (
-      <View style={style.viewBody}>
-        <Text style={style.textIntroduce}>
+      <View style={styles.container}>
+        <Text style={styles.text}>
           This app helps you determine if a certain specific recipe 
           based food include any IGR certified ingredients. To do this the App 
           will ask you for the Barcode (GS1 GTIN-13) on the wrapping and the 
           latest date associoated with the production lot. Usually the date 
           is printed on the wrapper.
         </Text>
-        <Button
-          onPress={pageParams => {
-            this.props.navigation.navigate('readBarcode', pageParams); // (id_page, params)
-          }}
-          title="Read Barcode"
-          accessibilityLabel="Learn more about this purple button"
-        />
+        <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center',}}>
+          <TouchableOpacity
+            onPress={pageParams => {
+              this.props.navigation.navigate('readBarcode', pageParams); // (id_page, params)
+            }}
+            style = {styles.button}>
+            <Text style={styles.textButton}>
+              Read barcode <Icon name="camera" size={20} />
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -39,14 +44,30 @@ class Home extends React.Component {
   }
 }
 
-const style = StyleSheet.create({
-  viewBody: {
+const styles = StyleSheet.create({
+  container: {
     flex: 1,
   }, 
-  textIntroduce: {
+  text: {
     color: '#000',
     fontSize: 15,
     margin: 10
+  },
+  textButton: {
+    color: '#000',
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+  button: {
+    backgroundColor: '#DDDDDD',
+    width: 300,
+    height: 45,
+    borderColor: "transparent",
+    borderWidth: 0,
+    borderRadius: 45,
+    alignItems: 'center',
+    padding: 10
   }
 });
 
